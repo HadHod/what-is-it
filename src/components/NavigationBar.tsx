643fed878c9799a8db6565b133b739bd0e112405
HadHod <maciej.mlynarski@gmail.com>
2024-02-11 16:56:25 +0100

    tailwind added

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import PathConstants from '../routes/pathConstants';
-import './NavigationBar.scss';
 
 function NavigationBar(): ReactElement {
-  return <nav><Link to={PathConstants.HOME}>HOME</Link> | <Link to={PathConstants.HOME}>log / register</Link></nav>
+  return <nav className="p-6 border border-solid border-black flex justify-evenly">
+    <Link to={PathConstants.HOME}>HOME</Link> | <Link to={PathConstants.HOME}>log / register</Link>
+  </nav>
 }
 
 export default NavigationBar;
