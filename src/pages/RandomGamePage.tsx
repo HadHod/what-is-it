11f432521ead58917f7829c9f14317f27d89b61e
HadHod <maciej.mlynarski@gmail.com>
2024-02-14 21:13:24 +0100

    loader and small refactoring added

--- src/pages/RandomGamePage.tsx
+++ src/pages/RandomGamePage.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, RefObject, useState } from 'react';
 
 function RandomGamePage(): ReactElement {
   const [context, setContext] = useState<CanvasRenderingContext2D | null | undefined>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const canvas: RefObject<HTMLCanvasElement> = React.useRef<HTMLCanvasElement>(null);
 
@@ -22,10 +23,37 @@ function RandomGamePage(): ReactElement {
   }
 
   React.useEffect(() => {
-    setContext(canvas.current?.getContext('2d'));
-  }, []);
+    function drawImageActualSize(this: HTMLImageElement): void {
+      if (!canvas.current || !context) {
+        return;
+      }
 
-  return <canvas ref={canvas} height={600} width={800} className="border border-solid border-black" onClick={handleCanvasClick} />;
+      canvas.current.width = this.naturalWidth;
+      canvas.current.height = this.naturalHeight;
+      context.drawImage(this, 0, 0);
+    }
+
+    function loadImage(): void {
+      const image = new window.Image();
+      image.src = "https://konvajs.org/assets/yoda.jpg";
+      image.addEventListener('load', drawImageActualSize);
+    }
+
+    setContext(canvas.current?.getContext('2d'));
+
+    loadImage();
+
+    setIsLoading(false);
+  }, [context]);
+
+  return (
+    <div className='flex justify-center items-center h-full'>
+      {isLoading
+        ? "Loading..."
+        : <canvas ref={canvas} className="border border-solid border-black" onClick={handleCanvasClick} />
+      }
+    </div>
+  );
 }
 
 export default RandomGamePage;
